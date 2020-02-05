71839156d5ee57a3837933cc5cad9b762d844bb5
Alissa <alissacrane123@gmail.com>
2020-02-05 15:27:44 -0800

    Fix project destroy action

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
-  has_many :project_memberships, dependent: :destroy
+  has_many :project_memberships
   has_many :members, through: :project_memberships, source: :user
   belongs_to :owner, foreign_key: :owner_id, class_name: :User
 
